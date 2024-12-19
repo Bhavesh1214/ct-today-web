0eb7c0da9bcfdaba3bc4f1297a75ef8a0e12c809
Bhavesh1214 <khasiyabhaveshsinh@gmail.com>
2024-12-19 14:08:52 +0530

    [fix]

--- src/components/Icons/FacebookIcon.jsx
+++ src/components/Icons/FacebookIcon.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React from "react";
 // redux
 
 // icons
-import { IoLogoFacebook } from 'react-icons/io5';
+import { IoLogoFacebook } from "react-icons/io5";
 
 // mui
-import { IconButton, useTheme } from '@mui/material';
+import { IconButton, useTheme } from "@mui/material";
 export default function FacebookIcon({ isAdmin }) {
   const theme = useTheme();
-  const isLight = theme.palette.mode === 'light';
+  const isLight = theme.palette.mode === "light";
   return (
-    <IconButton name="setting-mode" size="medium" color={isAdmin ? 'default' : 'primary'}>
-      <IoLogoFacebook size={16} color={isLight ? theme.palette.primary.dark : theme.palette.primary.main} />
+    <IconButton
+      name="setting-mode"
+      size="medium"
+      color={isAdmin ? "default" : "primary"}
+    >
+      <IoLogoFacebook
+        size={16}
+        color={
+          isLight ? theme.palette.primary.dark : theme.palette.primary.main
+        }
+      />
     </IconButton>
   );
 }
--- src/components/Icons/TwitterIcon.jsx
+++ src/components/Icons/TwitterIcon.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React from "react";
 // redux
 
 // icons
-import { IoLogoTwitter } from 'react-icons/io5';
+import { IoLogoTwitter } from "react-icons/io5";
 
 // mui
-import { IconButton, useTheme } from '@mui/material';
+import { IconButton, useTheme } from "@mui/material";
 export default function TwitterIcon({ isAdmin }) {
   const theme = useTheme();
-  const isLight = theme.palette.mode === 'light';
+  const isLight = theme.palette.mode === "light";
   return (
-    <IconButton name="setting-mode" size="medium" color={isAdmin ? 'default' : 'primary'}>
-      <IoLogoTwitter size={16} color={isLight ? theme.palette.primary.dark : theme.palette.primary.main} />
+    <IconButton
+      name="setting-mode"
+      size="medium"
+      color={isAdmin ? "default" : "primary"}
+    >
+      <IoLogoTwitter
+        size={16}
+        color={
+          isLight ? theme.palette.primary.dark : theme.palette.primary.main
+        }
+      />
     </IconButton>
   );
 }
